76dc8dc27fe70b1c2c6612609a1a06e198e49cc2
Des Whelan <wheland6@tcd.ie>
2025-10-30 19:39:58 +0000

    create csv file of un-guessed states when user exits game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import turtle
-
 import pandas
+import turtle
 
 ALIGN = "center"
 BG_IMAGE_PATH = "blank_states_img.gif"
@@ -11,12 +10,12 @@ STATE_DATA_PATH = "50_states.csv"
 screen = turtle.Screen()
 screen.title(GAME_TITLE)
 screen.addshape(BG_IMAGE_PATH)
-
 turtle.shape(BG_IMAGE_PATH)
 
 state_data = pandas.read_csv(STATE_DATA_PATH)
 
 user_score = 0
+state_names_list = state_data.name.tolist()
 correct_guesses = []
 
 def display_state_label(state):
@@ -26,17 +25,21 @@ def display_state_label(state):
     label.goto((state.x.item(), state.y.item()))
     label.write(state.name.item(), align=ALIGN, font=FONT)
 
-print(state_data)
-
 while user_score < 50:
     user_guess = screen.textinput(title=f"Score: {user_score}/50", prompt="Enter your guess: ").lower()
+
+    if user_guess == "exit":
+        states_missed_dictionary = {
+            "name": list(set(state_names_list) - set(correct_guesses))
+        }
+
+        states_missed_df = pandas.DataFrame(states_missed_dictionary)
+
+        states_missed_df.to_csv("states_to_learn.csv")
+        break
 
     for state_name in state_data.name:
         if state_name.lower() == user_guess and user_guess not in correct_guesses:
             user_score += 1
-            correct_guesses.append(user_guess)
+            correct_guesses.append(user_guess.title())
             display_state_label(state_data[state_data.name == state_name])
-
-print("Congratulations, YOU WIN!")
-
-turtle.mainloop()
